29cfa9a48f607ac518231295ca07dbe7b9208cc0
AlexandreCalmonJr <alexandrecalmonjunior@gmail.com>
2026-08-16 09:36:44 -0300

    Iniciando Projeto Restaurante

---
+++ src/main/java/org/example/Restaurante.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Restaurante {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao Sistema Sabor e Arte");
+        System.out.println("---Cardapio--Sabor e Arte----");
+        System.out.println("1 - Picanha       R$ 25,00");
+        System.out.println("2 - Lasanha       R$ 20,00");
+        System.out.println("3 - Strogonoff    R$ 18,00");
+        System.out.println("4 - Bife acebolado R$ 15,00");
+        System.out.println("5 - Pão com ovo   R$ 5,00");
+
+        System.out.println("Digite o código do prato:");
+        int codigo = scanner.nextInt();
+
+
+
+
+    }
+}
