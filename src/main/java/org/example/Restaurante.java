5016fccfed57c49872f0ef9a6e2e2d0bdcee62b9
AlexandreCalmonJr <alexandrecalmonjunior@gmail.com>
2026-08-16 15:31:57 -0300

    fix: valida entradas do pedido

--- src/main/java/org/example/Restaurante.java
+++ src/main/java/org/example/Restaurante.java
@@ -3,51 +3,72 @@ package org.example;
 import java.util.Scanner;
 
 public class Restaurante {
-    static void main() {
+
+    public static void main() {
+
         Scanner scanner = new Scanner(System.in);
+
         double total = 0;
         char continuar;
 
         do {
+
             System.out.println("Bem vindo ao Sistema Sabor e Arte");
             System.out.println("---Cardapio--Sabor e Arte----");
-            System.out.println("1 - Picanha       R$ 25,00");
-            System.out.println("2 - Lasanha       R$ 20,00");
-            System.out.println("3 - Strogonoff    R$ 18,00");
+            System.out.println("1 - Picanha        R$ 25,00");
+            System.out.println("2 - Lasanha        R$ 20,00");
+            System.out.println("3 - Strogonoff     R$ 18,00");
             System.out.println("4 - Bife acebolado R$ 15,00");
-            System.out.println("5 - Pão com ovo   R$ 5,00");
+            System.out.println("5 - Pão com ovo    R$ 5,00");
 
             System.out.println("Digite o código do prato:");
-            int codigo = scanner.nextInt();
 
+            while (!scanner.hasNextInt()) {
+                System.out.println("Entrada inválida. Digite um número de 1 a 5:");
+                scanner.next();
+            }
+
+            int codigo = scanner.nextInt();
 
             switch (codigo) {
                 case 1:
                     System.out.println("Você escolheu Picanha");
                     total += 25;
                     break;
+
                 case 2:
                     System.out.println("Você escolheu Lasanha");
                     total += 20;
                     break;
+
                 case 3:
                     System.out.println("Você escolheu Strogonoff");
                     total += 18;
                     break;
+
                 case 4:
                     System.out.println("Você escolheu Bife acebolado");
                     total += 15;
                     break;
+
                 case 5:
-                    System.out.println("Você escolheu Pão com ovo ");
+                    System.out.println("Você escolheu Pão com ovo");
                     total += 5;
                     break;
+
                 default:
-                    System.out.println("Codigo Invalido");
+                    System.out.println("Código inválido.");
             }
 
-            System.out.println("Deseja adicionar outro prato? (S/N)");
-            continuar = scanner.next().toUpperCase().charAt(0);
+            do {
+                System.out.println("Deseja adicionar outro prato? (S/N)");
+                continuar = scanner.next().toUpperCase().charAt(0);
+
+                if (continuar != 'S' && continuar != 'N') {
+                    System.out.println("Opção inválida. Digite apenas S ou N.");
+                }
+
+            } while (continuar != 'S' && continuar != 'N');
 
         } while (continuar == 'S');
 
@@ -55,5 +76,4 @@ public class Restaurante {
 
         scanner.close();
     }
-
 }
