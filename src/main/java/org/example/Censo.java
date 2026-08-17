e13b95612b58fbb16422fa006948b756085e0361
AlexandreCalmonJr <87951095+AlexandreCalmonJr@users.noreply.github.com>
2026-08-17 16:10:56 -0300

    feat: cria estrutura inicial do censo

---
+++ src/main/java/org/example/Censo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Censo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== CENSO SOCIOECONÔMICO =====");
+        System.out.println("1 - Adicionar pessoa");
+        System.out.println("2 - Exibir resultados");
+        System.out.println("3 - Sair");
+
+        scanner.close();
+    }
+}
